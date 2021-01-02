4e76d0c43d23fd298956191e4b00733489fbdb77
EladGashri <eladgashri@gmail.com>
2021-01-02 07:12:22 +0200

    Update

--- preprocessing.py
+++ preprocessing.py
@@ -1,14 +1,11 @@
-import numpy.core.multiarray
 import cv2 as cv
 import keras
 import numpy as np
+import pandas as pd
 import os #enables getting file names from directory
 import random
 import face_recognition #run this only after installing dlib, cmake and face_recognition
-import pymongo
-import bson
 import math
-
 
 #classes for train set images, validation set images and test set images
 class image:
@@ -17,7 +14,7 @@ class image:
         self.values=cv.imread(path)
         self.path=path
         self.dir=self.path.split('/')[-2]
-        self.person=(' ').join(self.dir.split('_'))
+        self.person=' '.join(self.dir.split('_'))
         self.file_name=self.path.split('/')[-1]
 
     def save(self,new_path,remove_old=False):
@@ -189,179 +186,122 @@ datagen = keras.preprocessing.image.ImageDataGenerator(
     preprocessing_function=preprocessing_for_augmentation)
 
 
+if __name__ == "__main__":
 
-dataset_dir = 'C:/Users/gash5/Desktop/dataset'
-os.chdir(dataset_dir)
-directories = [dir for dir in os.listdir(dataset_dir) if not '.' in dir] #directories contain all the people that have images in the dataset
+    dataset_dir = 'C:/Users/gash5/Desktop/dataset'
+    os.chdir(dataset_dir)
+    directories = [dir for dir in os.listdir(dataset_dir) if not '.' in dir] #directories contain all the people that have images in the dataset
 
-#new direstories are being made for the train, validation and test sets
-train_dir = dataset_dir + '/train'
-validation_dir = dataset_dir + '/validation'
-test_dir = dataset_dir + '/test'
-if not os.path.isdir(train_dir):
-    os.mkdir(train_dir)
-if not os.path.isdir(validation_dir):
-    os.mkdir(validation_dir)
-if not os.path.isdir(test_dir):
-    os.mkdir(test_dir)
+    #new direstories are being made for the train, validation and test sets
+    train_dir = ''.join([dataset_dir, '/train'])
+    validation_dir = ''.join([dataset_dir, '/validation'])
+    test_dir = ''.join([dataset_dir, '/test'])
+    if not os.path.isdir(train_dir):
+        os.mkdir(train_dir)
+    if not os.path.isdir(validation_dir):
+        os.mkdir(validation_dir)
+    if not os.path.isdir(test_dir):
+        os.mkdir(test_dir)
 
-no_faces_detected=[]
+    no_faces_detected=[]
 
-for dir in directories:
-    dir_path=dataset_dir + '/' + dir
-    files = os.listdir(dir_path)
-    images=[file for file in files if ((len(file.split('.')) == 2) and (file.split('.')[1] in ['jpg', 'jpeg', 'png']))] #contains all the images of the person in the current directory
-    if len(images)<2:
-        continue
+    for dir in directories:
+        dir_path=''.join([dataset_dir, '/', dir])
+        files = os.listdir(dir_path)
+        images=[file for file in files if ((len(file.split('.')) == 2) and (file.split('.')[1] in ['jpg', 'jpeg', 'png']))] #contains all the images of the person in the current directory
+        if len(images)<2:
+            continue
 
-    #if a person has less than 3 images he will not be part of any set
-    #if a person has 3 images all the images will be put in the train set
-    #if a person has 4 images all the images except one will he put in the train set and the last one will be put on the validation set
-    #if a person has more than 4 images  all the images except two will he put in the train set and the last two will be put on the validation set and test set
+        #if a person has less than 3 images he will not be part of any set
+        #if a person has 3 images all the images will be put in the train set
+        #if a person has 4 images all the images except one will he put in the train set and the last one will be put on the validation set
+        #if a person has more than 4 images  all the images except two will he put in the train set and the last two will be put on the validation set and test set
 
-    for image_name in images:
-        cur_image=image(dir_path + '/' + image_name)
-        face=cur_image.detect_face()
-        if (face is None):
-            images.remove(image_name)
-            print("No face detected for " + cur_image.person)
-            no_faces_detected.append(cur_image.path)
+        for image_name in images:
+            cur_image=image(''.join([dir_path, '/', image_name]))
+            face=cur_image.detect_face()
+            if face is None:
+                images.remove(image_name)
+                print(' '.join(["No face detected for",cur_image.person]))
+                no_faces_detected.append(cur_image.path)
 
-    train_set=[]
-    test_set = []
-    validation_set = []
-    if len(images)==2:
-        train_set.append(train_image(dir_path + '/' + images[0]))
-        test_set.append(test_image(dir_path + '/' + images[1]))
-    elif len(images)==3:
-        train_set.append(train_image(dir_path + '/' + images[0]))
-        test_set.append(test_image(dir_path + '/' + images[1]))
-        validation_set.append(validation_image(dir_path + '/' + images[2]))
-    elif len(images)>3:
-        train_set=[train_image(dir_path + '/' + cur_image) for cur_image in images[0:math.floor(len(images)*0.5)]]
-        test_set=[test_image(dir_path + '/' + cur_image) for cur_image in images[math.floor(len(images)*0.5):math.floor(len(images)*0.75)]]
-        validation_set=[validation_image(dir_path + '/' + cur_image) for cur_image in images[math.floor(len(images)*0.75):]]
+        cur_train_set=[]
+        cur_test_set = []
+        cur_validation_set = []
+        if len(images)==2:
+            cur_train_set.append(train_image(''.join([dir_path, '/', images[0]])))
+            cur_test_set.append(test_image(''.join([dir_path, '/', images[1]])))
+        elif len(images)==3:
+            cur_train_set.append(train_image(''.join([dir_path, '/', images[0]])))
+            cur_test_set.append(test_image(''.join([dir_path, '/', images[1]])))
+            cur_validation_set.append(validation_image(''.join([dir_path, '/', images[2]])))
+        elif len(images)>3:
+            cur_train_set=[train_image(''.join([dir_path, '/', cur_image])) for cur_image in images[0:math.floor(len(images)*0.5)]]
+            cur_test_set=[test_image(''.join([dir_path, '/', cur_image])) for cur_image in images[math.floor(len(images)*0.5):math.floor(len(images)*0.75)]]
+            cur_validation_set=[validation_image(''.join([dir_path, '/', cur_image])) for cur_image in images[math.floor(len(images)*0.75):]]
 
-#every image that goes to the train set generates 5 new augmentad images
-#the image face locations are saved by the method detect_face
+    #every image that goes to the train set generates 5 new augmentad images
+    #the image face locations are saved by the method detect_face
 
-    for new_train_image in train_set:
-        if not os.path.isdir(train_dir + '/' + dir):
-            os.mkdir(train_dir + '/' + dir)
-        new_train_image.preprocess()
-        new_path=train_dir + '/' + dir + '/' + image_name
-        train_image.train_paths_list[train_image.train_paths_list.index(new_train_image.path)]=new_path
-        new_train_image.save(new_path)
+        for new_train_image in cur_train_set:
+            new_train_dir=''.join([train_dir, '/', dir])
+            if not os.path.isdir(new_train_dir):
+                os.mkdir(new_train_dir)
+            new_train_image.preprocess()
+            new_path=''.join([train_dir, '/', dir, '/', image_name])
+            train_image.train_paths_list[train_image.train_paths_list.index(new_train_image.path)]=new_path
+            new_train_image.save(new_path)
 
-        image_for_aug = new_train_image.values.reshape((1,) + new_train_image.values.shape)
-        i=0
-        for batch in datagen.flow(image_for_aug,
-                                    batch_size=1,
-                                    save_to_dir=train_dir + '/' + dir,
-                                    save_prefix='aug',
-                                    save_format='jpg'):
-            i += 1
-            if i == 5:
-                break
+            image_for_aug = new_train_image.values.reshape((1,) + new_train_image.values.shape)
+            i=0
+            for batch in datagen.flow(image_for_aug,
+                                        batch_size=1,
+                                        save_to_dir=train_dir + '/' + dir,
+                                        save_prefix='aug',
+                                        save_format='jpg'):
+                i += 1
+                if i == 5:
+                    break
 
-    for new_test_image in test_set:
-        if not os.path.isdir(test_dir + '/' + dir):
-            os.mkdir(test_dir + '/' + dir)
-        new_path = test_dir + '/' + dir + '/' + image_name
-        new_test_image.preprocess()
-        test_image.test_paths_list[test_image.test_paths_list.index(new_test_image.path)] = new_path
-        new_test_image.save(new_path)
-
-
-    for new_validation_image in validation_set:
-        if not os.path.isdir(validation_dir + '/' + dir):
-            os.mkdir(validation_dir + '/' + dir)
-        new_validation_image.preprocess()
-        new_path=validation_dir + '/' + dir + '/' + image_name
-        validation_image.validation_paths_list[validation_image.validation_paths_list.index(new_validation_image.path)]=new_path
-        new_validation_image.save(new_path)
-
-#all the augmented values are collected and go through face detection and normalization like the other images
-train_directories = [dir for dir in os.listdir(train_dir) if not '.' in dir]
-for dir in train_directories:
-    dir_path=train_dir + '/' + dir
-    files = os.listdir(dir_path)
-    augmentation_images=[file for file in files if ((len(file.split('.'))==2) and (file.split('.')[1] in ['jpg', 'jpeg', 'png']) and file.split('_')[0]=='aug') ]
-
-    for cur_image in augmentation_images:
-        new_augmentation_image=augmentation_image(dir_path + '/' + cur_image)
-        face=new_augmentation_image.detect_face()
-        if face is None:
-            augmentation_image.augmentation_paths_list.remove(new_augmentation_image.path)
-            os.remove(new_augmentation_image.path)
-
-normalized_values=[]
-labels=[]
-whole_train_set=sum([train_image.train_paths_list,augmentation_image.augmentation_paths_list],[])
-
-for set in [whole_train_set,validation_image.validation_paths_list,test_image.test_paths_list]:
-    for image_name in set:
-        cur_image = image(image_name)
-        cur_image.values = cur_image.detect_face()
-        normalized_values.append(cur_image.normalize())
-        labels.append(cur_image.person)
-
-if not os.path.isdir(dataset_dir + '/' + 'no_faces_detected'):
-    os.mkdir(dataset_dir + '/' + 'no_faces_detected')
-for image_path in no_faces_detected:
-    no_face_image=image(image_path)
-    no_face_image.save(dataset_dir + '/' + 'no_faces_detected' + '/' + no_face_image.file_name)
-
-'''''
-#all the images in the train, validation and test sets go through normalization
-#the normalization type is standardization that is done by substracting the train set mean and dividing by the train set STD
-#the normalized values are calculated by the normalize mothod
-
-train_documents=[]
-for cur_image in sum([train_image.train_paths_list,augmentation_image.augmentation_paths_list],[]):
-    cur_train_image=image(cur_image)
-    cur_train_image.values=cur_train_image.detect_face()
-    normalized_values=cur_train_image.normalize()
-    train_documents.append(bson.son.SON({
-    "values":cur_train_image.values.tolist(),
-    "normalized_values":normalized_values.tolist(),
-    "person":cur_train_image.person,
-    "path":cur_train_image.path,
-    "set":"train"
-    }))
-
-validation_documents=[]
-for cur_image in validation_image.validation_paths_list:
-    cur_validation_image=image(cur_image)
-    cur_validation_image.values=cur_validation_image.detect_face()
-    normalized_values=cur_validation_image.normalize()
-    validation_documents.append(bson.son.SON({
-    "values":cur_train_image.values.tolist(),
-    "normalized_values":normalized_values.tolist(),
-    "person":cur_train_image.person,
-    "path":cur_validation_image.path,
-    "set":"validation"
-    }))
-
-test_documents=[]
-for cur_image in test_image.test_paths_list:
-    cur_test_image=image(cur_image)
-    cur_test_image.values=cur_test_image.detect_face()
-    normalized_values=cur_test_image.normalize()
-    test_documents.append(bson.son.SON({
-    "values":cur_train_image.values.tolist(),
-    "normalized_values":normalized_values.tolist(),
-    "person":cur_train_image.person,
-    "path":cur_test_image.path,
-    "set":"test"
-    }))
+        for new_test_image in cur_test_set:
+            new_test_dir=''.join([test_dir, '/', dir])
+            if not os.path.isdir(new_test_dir):
+                os.mkdir(new_test_dir)
+            new_path = ''.join([test_dir, '/', dir, '/', image_name])
+            new_test_image.preprocess()
+            test_image.test_paths_list[test_image.test_paths_list.index(new_test_image.path)] = new_path
+            new_test_image.save(new_path)
 
 
-client = pymongo.MongoClient('mongodb://localhost:27017/')
-with client:
-    db = client.biometric_system
-    db.faces.insert_many(train_documents)
-    db.faces.insert_many(validation_documents)
-    db.faces.insert_many(test_documents)
-'''''
+        for new_validation_image in cur_validation_set:
+            new_validation_dir=''.join([validation_dir, '/', dir])
+            if not os.path.isdir(new_validation_dir):
+                os.mkdir(new_validation_dir)
+            new_validation_image.preprocess()
+            new_path=''.join([validation_dir, '/', dir, '/', image_name])
+            validation_image.validation_paths_list[validation_image.validation_paths_list.index(new_validation_image.path)]=new_path
+            new_validation_image.save(new_path)
 
+    train_directories = [dir for dir in os.listdir(train_dir) if not '.' in dir]
+    for dir in train_directories:
+        dir_path=''.join([train_dir, '/', dir])
+        files = os.listdir(dir_path)
+        augmentation_images=[file for file in files if ((len(file.split('.'))==2) and (file.split('.')[1] in ['jpg', 'jpeg', 'png']) and file.split('_')[0]=='aug') ]
+
+        for cur_image in augmentation_images:
+            new_augmentation_image=augmentation_image(''.join([dir_path, '/', cur_image]))
+            face=new_augmentation_image.detect_face()
+            if face is None:
+                augmentation_image.augmentation_paths_list.remove(new_augmentation_image.path)
+                os.remove(new_augmentation_image.path)
+
+    no_faces_detected_dir=''.join([dataset_dir, '/', 'no faces detected'])
+    if not os.path.isdir(no_faces_detected_dir):
+        os.mkdir(no_faces_detected_dir)
+    for image_path in no_faces_detected:
+        no_face_image=image(image_path)
+        no_face_image.save(''.join([no_faces_detected_dir, '/', no_face_image.file_name]))
+
+    train_set=sum([train_image.train_paths_list,augmentation_image.augmentation_paths_list],[])
+    validation_set=validation_image.validation_paths_list
+    test_set=train_image.train_paths_list
