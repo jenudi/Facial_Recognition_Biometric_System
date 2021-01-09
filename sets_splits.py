7bda2ddf79030a6559cf877c5ca3d095cf63c17f
EladGashri <eladgashri@gmail.com>
2021-01-09 07:08:00 +0200

    Update

--- sets_splits.py
+++ sets_splits.py
@@ -1,114 +1,83 @@
 import os
 import math
-import sys
-from face_recognition import face_locations #run this only after installing dlib, cmake and face_recognition
+import pandas as pd
+from face_recognition import face_locations
 from augmentation import *
-
 import cv2 as cv
 import numpy as np
+import tensorflow as tf
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 #%% classes for train set images, validation set images and test set images
 class image_in_set:
 
-    def __init__(self,values=None,path=None):
-
+    def __init__(self,path):
         self.values=cv.imread(path)
         self.path=path
         self.dir=self.path.split('\\')[-2]
         self.person=' '.join(self.dir.split('_'))
         self.file_name=self.path.split('\\')[-1]
 
-    def save(self,new_path):
-        if not type(self).__name__=='image_in_set':
-            class_paths_list=getattr(sys.modules[__name__], type(self).__name__).paths_list
-            getattr(sys.modules[__name__], type(self).__name__).paths_list[class_paths_list.index(self.path)] = new_path
-        self.path=new_path
-        cv.imwrite(self.path,self.values)
-
-    def detect_face(self):
+    def get_face_image(self):
         face_loc=face_locations(self.values)
         try:
             face=self.values[face_loc[0][0]:face_loc[0][1],face_loc[0][3]:face_loc[0][2]]
         except IndexError:
             return None
-        if (not face is None) and (len(face)>0):
-            return face
+        if (not face is None) and len(face):
+            return face_image(face,self.person)
         else:
             return None
 
-    def normalize(self):
-        return (self.values - train_image.get_train_mean()) / train_image.get_train_std()
+    def normalize(self,paths_list):
+        images_mean = get_images_mean(paths_list)
+        images_std = get_images_std(paths_list)
+        return (self.values - images_mean) / images_std
 
-    def preprocess(self):
-        self.values = cv.resize(self.values, (256, 256))
+    def save(self, new_path):
+        self.path = new_path
+        cv.imwrite(self.path, self.values)
+
+
+    def resize_image(self):
+        self.values = cv.resize(self.values, (160, 160))
+
+
+class face_image(image_in_set):
+
+    def __init__(self,values,person):
+        self.values=values
+        self.person=person
+        self.path=None
+
+
+def get_images_mean(paths_list):
+    if len(paths_list):
+        train_mean=[]
+        for path in paths_list:
+            train_image=image_in_set(path)
+            train_mean.append(train_image.values)
+            return np.mean(train_mean,axis=(0,1,2))
+    else:
+        raise ValueError("No images")
+
+def get_images_std(paths_list):
+    if len(paths_list):
+        train_std=[]
+        for path in paths_list:
+            train_image=image_in_set(path)
+            train_std.append(train_image.values)
+            return np.std(train_std,axis=(0,1,2))
+    else:
+        raise ValueError("No images")
 
 
 #all the train set images are saved in a list which is a class variable. this list is used in order or extract the mean and std of the
 #train set images for normalization for the rest of the images
 
-class train_image(image_in_set):
 
-    paths_list=[]
-
-    def __init__(self,path):
-        image_in_set.__init__(self,path)
-
-        train_image.paths_list.append(path)
-
-    @classmethod
-    def get_train_mean(cls):
-        if len(cls.paths_list)>0:
-            train_mean=[]
-            for train_path in cls.paths_list:
-                train_image=image_in_set(train_path)
-                train_mean.append(train_image.values)
-                return np.mean(train_mean,axis=(0,1,2))
-        else:
-            raise ValueError("No train images")
-
-    @classmethod
-    def get_train_std(cls):
-        if len(cls.paths_list)>0:
-            train_std=[]
-            for train_path in cls.paths_list:
-                train_image=image_in_set(train_path)
-                train_std.append(train_image.values)
-                return np.std(train_std,axis=(0,1,2))
-        else:
-            raise ValueError("No train images")
-
-class augmentation_image(image_in_set):
-
-    paths_list=[]
-
-    def __init__(self,path):
-        image_in_set.__init__(self,path)
-
-        augmentation_image.paths_list.append(path)
-
-
-class validation_image(image_in_set):
-
-    paths_list=[]
-
-    def __init__(self,path):
-        image_in_set.__init__(self,path)
-
-        validation_image.paths_list.append(path)
-
-
-class test_image(image_in_set):
-
-    paths_list=[]
-
-    def __init__(self,path):
-        image_in_set.__init__(self,path)
-
-        test_image.paths_list.append(path)
-
-
-def main(augmentation_num=10):
+if __name__ == "__main__":
 
     dataset_dir=input("Please enter the dataset directory path")
     os.chdir(dataset_dir)
@@ -125,7 +94,13 @@ def main(augmentation_num=10):
     if not os.path.isdir(test_dir):
         os.mkdir(test_dir)
 
+    train_paths = list()
+    validation_paths = list()
+    test_paths = list()
+    augmentation_paths = list()
     no_faces_detected = list()
+
+    augmentation_num = 10
 
     for dir in directories:
         dir_path=''.join([dataset_dir, '\\', dir])
@@ -134,13 +109,14 @@ def main(augmentation_num=10):
         if len(images)<2:
             continue
 
+
         #if a person has less than 2 images he will not be part of any set
         #if a person has 2 images all the images will be put in the train set and test set
         #if a person has 3 images 1 image will be in the train, validation and test set
         #if a person has more than 3 50 % of the images will be in the train set and 25% and 25% will be in the validation and test set
         for image_name in images:
             cur_image=image_in_set(''.join([dir_path, '\\', image_name]))
-            face=cur_image.detect_face()
+            face=cur_image.get_face_image()
             if face is None:
                 images.remove(image_name)
                 print(' '.join(["No face detected for",cur_image.person]))
@@ -163,17 +139,18 @@ def main(augmentation_num=10):
             cur_test_set=[image_in_set(''.join([dir_path, '\\', cur_image])) for cur_image in images[round(len(images) * 0.75):]]
 
         #every image that goes to the train set generates 5 new augmentad images
-        #the image face locations are saved by the method detect_face
+        #the image face locations are saved by the method get_face_image
         for new_train_image in cur_train_set:
             new_train_dir=''.join([train_dir, '\\', dir])
 
             if not os.path.isdir(new_train_dir):
                 os.mkdir(new_train_dir)
-            face = new_train_image.detect_face()
-            new_train_image.preprocess()
+            new_face_image = new_train_image.get_face_image()
+            new_face_image.resize_image()
             new_path=''.join([train_dir, '\\', dir, '\\', new_train_image.file_name])
-            cv.imwrite(face,new_path)
-            train_image(new_path)
+            new_face_image.save(new_path)
+            train_paths.append(new_path)
+
             image_for_aug = new_train_image.values.reshape((1,) + new_train_image.values.shape)
             i=0
             for batch in datagen.flow(image_for_aug, batch_size=1, save_to_dir=new_train_dir, save_prefix='aug', save_format='jpg'):
@@ -185,19 +162,21 @@ def main(augmentation_num=10):
             new_validation_dir=''.join([validation_dir, '\\', dir])
             if not os.path.isdir(new_validation_dir):
                 os.mkdir(new_validation_dir)
-            face = new_validation_image.detect_face()
-            new_validation_image.preprocess()
+            new_face_image = new_validation_image.get_face_image()
+            new_face_image.resize_image()
             new_path=''.join([validation_dir, '\\', dir, '\\', new_validation_image.file_name])
-            cv.imwrite(face,new_path)
-            validation_image(new_path)
+            new_face_image.save(new_path)
+            validation_paths.append(new_path)
 
         for new_test_image in cur_test_set:
             new_test_dir=''.join([test_dir, '\\', dir])
             if not os.path.isdir(new_test_dir):
                 os.mkdir(new_test_dir)
             new_path = ''.join([test_dir, '\\', dir, '\\', new_test_image.file_name])
-            new_test_image.preprocess()
-            new_test_image.save(new_path)
+            new_face_image.resize_image()
+            new_path=''.join([test_dir, '\\', dir, '\\', new_train_image.file_name])
+            new_face_image.save(new_path)
+            test_paths.append(new_path)
 
     train_directories = [dir for dir in os.listdir(train_dir) if not '.' in dir]
     for dir in train_directories:
@@ -206,11 +185,13 @@ def main(augmentation_num=10):
         augmentation_images=[file for file in files if ((len(file.split('.'))==2) and (file.split('.')[1] in ['jpg', 'jpeg', 'png']) and file.split('_')[0]=='aug') ]
 
         for cur_image in augmentation_images:
-            new_augmentation_image=augmentation_image(''.join([dir_path, '\\', cur_image]))
-            face=new_augmentation_image.detect_face()
-            if face is None:
-                augmentation_image.paths_list.remove(new_augmentation_image.path)
-                os.remove(new_augmentation_image.path)
+            new_augmentation_image=image_in_set(''.join([dir_path, '\\', cur_image]))
+            new_face_image=new_augmentation_image.get_face_image()
+            os.remove(new_augmentation_image.path)
+            if not new_face_image is None:
+                new_face_image.resize_image()
+                new_face_image.save(new_augmentation_image.path)
+                augmentation_paths.append(new_face_image.path)
 
     no_faces_detected_dir=''.join([dataset_dir, '\\', 'no faces detected'])
     if not os.path.isdir(no_faces_detected_dir):
@@ -219,7 +200,30 @@ def main(augmentation_num=10):
         no_face_image=image_in_set(image_path)
         no_face_image.save(''.join([no_faces_detected_dir, '\\', no_face_image.file_name]))
 
-main()
-train_set=sum([train_image.paths_list,augmentation_image.paths_list],[])
-validation_set=validation_image.paths_list
-test_set=test_image.paths_list
+    #all the images in the train, validation and test sets go through normalization
+    #the normalization type is standardization that is done by substracting the train set mean and dividing by the train set STD
+    #the normalized values are calculated by the normalize mothod
+    model_name='facenet_keras.h5'
+    model = tf.keras.models.load_model(model_name,compile=False)
+
+    whole_train_paths = sum([train_paths, augmentation_paths], [])
+    train_df=pd.DataFrame(columns=['embedding', 'label'])
+    for index,image_path in enumerate(whole_train_paths):
+        cur_image = image_in_set(image_path)
+        norm_values=cur_image.normalize(train_paths).astype("float32")
+        four_dim_values=np.expand_dims(norm_values, axis=0)
+        train_df.loc[index]=[model.predict(four_dim_values)[0],cur_image.person]
+
+    validation_df=pd.DataFrame(columns=['embedding', 'label'])
+    for index,image_path in enumerate(validation_paths):
+        cur_image = image_in_set(image_path)
+        norm_values=cur_image.normalize(train_paths).astype("float32")
+        four_dim_values=np.expand_dims(norm_values, axis=0)
+        validation_df.loc[index]=[model.predict(four_dim_values)[0],cur_image.person]
+
+    test_df=pd.DataFrame(columns=['embedding', 'label'])
+    for index,image_path in enumerate(test_paths):
+        cur_image = image_in_set(image_path)
+        norm_values=cur_image.normalize(train_paths).astype("float32")
+        four_dim_values=np.expand_dims(norm_values, axis=0)
+        test_df.loc[index]=[model.predict(four_dim_values)[0],cur_image.person]
